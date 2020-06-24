f47bed4e00877c5a38a2c87b74a7e72981df33dd
David Slayback <david.slayback2010@gmail.com>
2020-06-24 11:46:13 -0400

    More work on the basic model class. Would ideally like it to not need to know the current option, but not quite sure how to approach

--- rllib/contrib/A2OC/Models/torch/oc_vision.py
+++ rllib/contrib/A2OC/Models/torch/oc_vision.py
@@ -6,16 +6,39 @@ from ray.rllib.models.torch.misc import normc_initializer, same_padding, \
 from ray.rllib.utils.annotations import override
 from ray.rllib.utils.framework import get_activation_fn, try_import_torch
 
+import torch
 import torch.nn as nn
 #_, nn = try_import_torch()
 
-# Atari filters as in https://arxiv.org/pdf/1609.05140.pdf
+# Model config fields
+OCNET_DEFAULT_CONFIG = {
+    'oc_num_options': 4,
+    'oc_option_epsilon': 0.1
+}
+
+# Atari filters as in https://arxiv.org/pdf/1609.05140.pdf (Original OC network, Nature CNN)
 OCNET_FILTERS = [
     [32, [8, 8], 4],
     [64, [4, 4], 2],
     [64, [3, 3], 1]
 ]
 OCNET_DENSE = 512
+
+# A3C Atari filters as in https://www.aaai.org/ocs/index.php/AAAI/AAAI18/paper/viewFile/17421/16660 (A2OC network)
+A2OCNET_FILTERS = [
+    [16, [8, 8], 4],
+    [32, [4, 4], 2]
+]
+A2OCNET_DENSE = 256
+
+# View layer abstraction so I can use Sequential
+class View(nn.Module):
+    def __init__(self, sizes):
+        super().__init__()
+        self.sizes = sizes
+
+    def forward(self, x):
+        return x.view(-1, *self.sizes)  # i.e., -1, num_options, num_outputs
 
 def _get_filter_config(shape):
     shape = list(shape)
@@ -48,61 +71,44 @@ class OptionCriticVisionNetwork(TorchModelV2, nn.Module):
             in_channels = out_channels
             in_size = out_size
         # Dense layer after flattening output, using ReLU
+        hSize = OCNET_DENSE
+        num_options = model_config.get('oc_num_options')
+        self.option_epsilon = model_config.get('oc_option_epsilon')
         layers.append(nn.Flatten())
-        layers.append(nn.Linear(in_size, OCNET_DENSE))
+        layers.append(nn.Linear(in_size, hSize))
         layers.append(nn.ReLU())
         self._convs = nn.Sequential(*layers)
-        vf_share_layers = model_config.get("vf_share_layers")
+        # Value function does share layers
 
         # Whether the last layer is the output of a Flattened (rather than
         # a n x (1,1) Conv2D).
         self.last_layer_is_flattened = True
         self._logits = None
 
-
-        # Build the value layers
-        self._value_branch_separate = self._value_branch = None
-        self._value_branch = SlimFC(
-            out_channels, 1, initializer=normc_initializer(0.01))
-
-        # Holds the current "base" output (before logits layer).
-        self._features = None
+        # q, pi, beta, and v
+        self.q = nn.Linear(hSize, num_options)  # Value for each option
+        self.v = nn.Linear(hSize, 1)  # Value for state alone? Or do
+        self.pi = nn.Sequential(nn.Linear(hSize, num_options * num_outputs), View((num_options, num_outputs)), nn.Softmax(dim=-1))  # Action probabilities for each option
+        self.beta = nn.Sequential(nn.Linear(hSize, num_options), nn.Sigmoid)  # Termination probabilities
+        # Holds the current "base" output (before heads).
+        self._features = self._q = self._v = self._pi = self._beta = None
 
     @override(TorchModelV2)
     def forward(self, input_dict, state, seq_lens):
         self._features = input_dict["obs"].float().permute(0, 3, 1, 2)
         conv_out = self._convs(self._features)
         # Store features to save forward pass when getting value_function out.
-        if not self._value_branch_separate:
-            self._features = conv_out
-
-        if not self.last_layer_is_flattened:
-            if self._logits:
-                conv_out = self._logits(conv_out)
-            logits = conv_out.squeeze(3)
-            logits = logits.squeeze(2)
-            return logits, state
-        else:
-            return conv_out, state
+        self._features = conv_out
+        self._q, self._pi, self._beta = self.q(self._features), self.pi(self._features), self.beta(self._features)
+        return self._pi, state  # self._pi has logits for each option, need to downselect to just correct option
 
     @override(TorchModelV2)
     def value_function(self):
-        assert self._features is not None, "must call forward() first"
-        if self._value_branch_separate:
-            value = self._value_branch_separate(self._features)
-            value = value.squeeze(3)
-            value = value.squeeze(2)
-            return value.squeeze(1)
-        else:
-            if not self.last_layer_is_flattened:
-                features = self._features.squeeze(3)
-                features = features.squeeze(2)
-            else:
-                features = self._features
-            return self._value_branch(features).squeeze(1)
+        assert self._q is not None, "must call forward() first"
+        eps = self.option_epsilon
+        self._v = torch.max(self._q, dim=-1) * (1-eps) + torch.mean(self._q, dim=1) * eps
+        return self._v
 
     def _hidden_layers(self, obs):
         res = self._convs(obs.permute(0, 3, 1, 2))  # switch to channel-major
-        res = res.squeeze(3)
-        res = res.squeeze(2)
         return res
